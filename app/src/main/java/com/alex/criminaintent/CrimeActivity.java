35393a1136fca018013cb0ef13d53764251f2df9
Sandroisu <sandroisu1411@gmail.com>
2019-11-15 17:03:02 +0300

    just started

--- app/src/main/java/com/alex/criminaintent/CrimeActivity.java
+++ app/src/main/java/com/alex/criminaintent/CrimeActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CrimeActivity extends AppCompatActivity {
-
+float x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
     }
+
 }
