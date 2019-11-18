b5464e9910ac6076ee6a879817fc3fa5ce396203
Sandroisu <sandroisu1411@gmail.com>
2019-11-18 16:45:10 +0300

    some changes

--- app/src/main/java/com/alex/criminaintent/CrimeFragment.java
+++ app/src/main/java/com/alex/criminaintent/CrimeFragment.java
@@ -23,6 +23,5 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
         return v;
-
     }
 }
