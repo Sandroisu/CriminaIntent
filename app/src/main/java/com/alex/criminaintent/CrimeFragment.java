c6a1614de1e4d54e9b8a473c40fcb6265585671c
Sandroisu <sandroisu1411@gmail.com>
2019-12-10 09:18:43 +0300

    Chapter16 allset

--- app/src/main/java/com/alex/criminaintent/CrimeFragment.java
+++ app/src/main/java/com/alex/criminaintent/CrimeFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -163,6 +164,7 @@ startActivityForResult(captureImage,REQUEST_PHOTO);
             }
         });
         mPhotoView = v.findViewById(R.id.crime_photo);
+        updatePhotoView();
         return v;
     }
 
@@ -213,6 +215,19 @@ startActivityForResult(captureImage,REQUEST_PHOTO);
             } finally {
                 c.close();
             }
+        }else if (requestCode == REQUEST_PHOTO){
+            Uri uri = FileProvider.getUriForFile(getActivity(),"com.alex.criminalintent.fileprovider", mPhotoFile);
+            getActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            updatePhotoView();
+        }
+    }
+
+    private void updatePhotoView() {
+        if (mPhotoFile == null||!mPhotoFile.exists()){
+            mPhotoView.setImageDrawable(null);
+        }else{
+            Bitmap bitmap =PictureUtils.getScaledBitmap(mPhotoFile.getPath(), getActivity());
+            mPhotoView.setImageBitmap(bitmap);
         }
     }
 
